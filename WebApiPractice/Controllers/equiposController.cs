b2b430afe755c811e249112f47e2af7beb29ae07
Natal <Natal@NATALY01>
2024-02-23 21:54:09 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPractice/Controllers/equiposController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPractice.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace WebApiPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+
+            
+
+            
+
+        }
+
+        //EndPoint que retorna el listado de todos los equipos existentes
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<equipos> ListadoEquipo = (from e in _equiposContexto.equipos 
+                                           select e).ToList();
+
+            if (ListadoEquipo.Count == 0)
+            {
+                return NotFound();
+            } 
+
+            return Ok(ListadoEquipo);
+
+        }
+
+    }
+
+
+}
